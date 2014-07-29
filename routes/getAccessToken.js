f629007c52ff16c02f968b1a35525da52ecaff2c
smilesj <smilesj@outlook.kr>
2014-07-29 05:25:45 +0100

    #3_sj

--- routes/getAccessToken.js
+++ routes/getAccessToken.js
@@ -2,22 +2,25 @@ var express = require('express');
 var router = express.Router();
 var request = require('request');
 
+var header_res = '';
+var r = request.get('https://apis.daum.net/oauth2/token', function(err, res, body) {
+	//for(var header in res.headers) {
+	//	header_res += (header+ " : " + res.headers[header] + '\n');
+	//}
+	header_res = res.headers;
+	console.log("GETACCESSTOKEN-HEADER");
+	console.log(header_res);
+
+});
+
 /* GET users listing. */
 router.post('/', function(req, res) {
-
-	//console.log('getAccessToken');
-	//console.log(req.body);
 
 	var grant_type = req.body.grant_type;
 	var authcode = req.body.code;
 	var client_id = req.body.client_id;
 	var client_secret = req.body.client_secret;
-	console.log(grant_type);
 
-	//var path = 'https://apis.daum.net/oauth2/token?grant_type='+grant_type+'&code='+authcode+'&client_id='+client_id+'&client_secret='+client_secret;
-	//request(path, function(error, response, body){
-	//	console.log(body.access_token);
-	//});
 	//access token 요청
 	request( {
 		method: 'POST',
@@ -30,7 +33,9 @@ router.post('/', function(req, res) {
 			redirect_uri : '/'
 		}
 	}, function(err, response, body) {
-		res.send(body);
+		var token = JSON.parse(body);
+		var value = {"header_res":header_res, "token": token};
+		res.send(JSON.stringify(value));
 	});
 });
 
